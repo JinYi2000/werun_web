e4dd5ad0b9d10335b8f117c6238505fa4015a884
JinYi2000 <1439261382@qq.com>
2020-09-16 22:51:55 +0800

    ok

--- werun_web/src/components/header.vue
+++ werun_web/src/components/header.vue
@@ -1,5 +1,17 @@
 <template>
 <div>
-    <img src = '@/assets/img/logo-werun.png'>
+    <img id='logo' src='@/assets/img/logo-werun.png'>
 </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+#logo{
+    width:50px;
+    height:50px;
+    
+}
+</style>
--- werun_web/src/components/homePage.vue
+++ werun_web/src/components/homePage.vue
@@ -1,18 +1,50 @@
 <template>
-<div>
-    <div id = 'header'>
+<div id='content'>
+    <div id='header'>
+        <myHeader></myHeader>
     </div>
-    <div id = 'pics'>
+    <div id='pics'>
     </div>
-    <div id = 'news'>
+    <div id='news'>
     </div>
-    <div id = 'fields'>
+    <div id='fields'>
     </div>
-    <div id = 'projects'>
+    <div id='projects'>
     </div>
-    <div id = 'joinUs'>
+    <div id='joinUs'>
     </div>
-    <div id = 'footer'>
+    <div id='footer'>
     </div>
 </div>
 </template>
+<script>
+import myHeader from '@/components/header'
+export default {
+    data(){
+
+    },
+    components:{
+        myHeader
+    }
+}
+</script>
+<style scoped>
+#header{
+    height: 170px;
+    width:100%;
+    border:1px solid rgb(47, 20, 202);
+    position:fixed;
+    top:0px;
+    
+}
+#content{
+    display:flex;
+    flex-direction:column;
+}
+#pics{
+    margin-top:170px;
+    height: 360px;
+    width:100%;
+    border:1px solid rgb(47, 20, 202);   
+}
+</style>
